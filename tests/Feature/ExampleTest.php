129d4c16f37e8c217e1fc529138ddbe151cc4c3b
KyokoToreno <kyokotoreno@proton.me>
2024-10-20 14:11:31 -0500

    Fixed testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /**
      * A basic test example.
      */
@@ -16,6 +16,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
 }
