b3d1c3f6b956ac10343b3825120e9bc39fba439c
KyokoToreno <kyokotoreno@proton.me>
2024-10-24 01:53:12 -0500

    Commented out placeholder paragraph from indexes

--- resources/js/Pages/Communities/Index.vue
+++ resources/js/Pages/Communities/Index.vue
@@ -9,7 +9,7 @@ defineProps({ communities: Array });
     <DefaultLayout>
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
             <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Explore Communities</h1>
-            <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here you can start your experience.</p>
+            <!--<p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here you can start your experience.</p>-->
 
             <div class="pt-16 px-4">
                 <ul class="flex flex-col gap-5">
